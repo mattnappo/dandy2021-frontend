61c35cac6fc9ea1f6b9700cd4f420945b5731fcd
xoreo <mattnappo@gmail.com>
2021-10-23 01:47:54 -0400

    Init.

---
+++ BrowseScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './styles.js'
+
+export default function BrowseScreen() {
+  return(
+    <View style={styles.container}>
+      <Text>This is the browse screen</Text>
+    </View>
+  );
+}
---
+++ DropScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './styles.js'
+
+export default function DropScreen() {
+  return(
+    <View style={styles.container}>
+      <Text>This is the drop screen</Text>
+    </View>
+  );
+}
