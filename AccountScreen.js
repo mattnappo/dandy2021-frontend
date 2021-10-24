f988f4838f2e3f0dd6eddebce6ef123c967f69a8
nathanielangafor <nathanielangafor@gmail.com>
2021-10-23 21:39:27 -0400

    more code

--- AccountScreen.js
+++ AccountScreen.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { styles } from './styles.js'
+import React, { useState } from 'react';
+import { Text, View, Button, Alert, TextInput} from 'react-native';
+import { styles } from './styles';
+import axios from 'axios';
+import 'localstorage-polyfill'; 
+
+
 
 export function AccountScreen() {
   return(
-    <View style={styles.container}>
-      <Text>This is the account screen</Text>
-    </View>
+    
+    <View style={styles.centerting}>
+      <Text>Account Information!</Text>
+
+      <Text> </Text>
+
+      <Text style={styles.input}>Username:</Text>
+      <Text style={styles.input}>Email:</Text>
+      <Text style={styles.input}>Points: </Text>
+      <Text style={styles.input}>Achievements: </Text>
+
+    </View>   
+    
+    
   );
 }
